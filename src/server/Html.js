86ab616ffa4cb24b6a52a0bef90a5139b4c9d9e4
Alexander J Ray <alexjamesray@gmail.com>
2017-02-27 16:51:04 +0100

    Adds vendor bundle

--- src/server/Html.js
+++ src/server/Html.js
@@ -54,8 +54,9 @@ class Html extends Component {
        </head>
        <body>
          <script dangerouslySetInnerHTML={{__html: initialState}} />
-         {PROD ? <div id="root" dangerouslySetInnerHTML={{__html: root}}></div> : <div id="root"></div>}
+          {PROD ? <div id="root" dangerouslySetInnerHTML={{__html: root}}></div> : <div id="root"></div>}
           {PROD && <script dangerouslySetInnerHTML={{__html: manifest.text}}/>}
+          {PROD && <script src={vendor.js}/>}
          <script src={PROD ? app.js : '/static/app.js'} />
        </body>
      </html>
